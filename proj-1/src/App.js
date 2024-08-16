5f4c8f6b96ae6d5ae6417a8c4218e5805e9034bf
VedulaLathasri <lathasri429@gmail.com>
2024-08-16 22:01:09 +0530

    functional and class components and state

--- proj-1/src/App.js
+++ proj-1/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Message/>
+        {/* <Welcome/>
+        <Greet/> */}
         <a
           className="App-link"
           href="https://reactjs.org"
