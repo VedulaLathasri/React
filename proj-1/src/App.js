5b3b21ad9affa11ecf5895fec83f8a88a1424ae6
VedulaLathasri <lathasri429@gmail.com>
2024-08-17 16:10:24 +0530

    counter.js program

--- proj-1/src/App.js
+++ proj-1/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message'
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Message/>
+        <Counter/>
+        {/* <Message/> */}
         {/* <Welcome/>
         <Greet/> */}
         <a
