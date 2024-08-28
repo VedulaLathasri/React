91861af09d492466e8c365477c703c0f794fffb7
VedulaLathasri <lathasri429@gmail.com>
2024-08-28 16:22:40 +0530

    component mounting and updating lifecycle methods

---
+++ componentlifecycle/src/components/LifecycleA.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import LifecycleB from './LifecycleB';
+
+class LifecycleA extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name:'Latha'
+      }
+      console.log('LifecycleA constructor');
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleA getDerivedStateFromProps');
+        return null;
+    }
+
+    shouldComponentUpdate() 
+    {
+      console.log('LifecycleA shouldComponentUpdate');
+      return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log('LifecycleA getSnapshotBeforeUpdate');
+    }
+
+    componentDidUpdate() {
+      console.log('LifecycleA componentDidUpdate');
+    }
+    
+    componentDidMount(){
+        console.log('LifecycleA componentDidMount ');
+    }
+    render() {
+        console.log('LifecycleA render method');
+    return (
+      <div>
+        <LifecycleB/>
+        <div>LifecycleA</div>
+      </div>
+    )
+  }
+}
+
+export default LifecycleA
---
+++ componentlifecycle/src/components/LifecycleB.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class LifecycleB extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name:'Latha'
+      }
+      console.log('LifecycleB constructor');
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleB getDerivedStateFromProps');
+        return null;
+    }
+
+    shouldComponentUpdate() 
+    {
+      console.log('LifecycleB shouldComponentUpdate');
+      return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log('LifecycleB getSnapshotBeforeUpdate');
+    }
+
+    componentDidUpdate() {
+      console.log('LifecycleB componentDidUpdate');
+    }
+
+    componentDidMount(){
+        console.log('LifecycleB componentDidMount ');
+    }
+    render() {
+        console.log('LifecycleB render method');
+    return (
+      <div>LifecycleB</div>
+    )
+  }
+}
+
+export default LifecycleB
