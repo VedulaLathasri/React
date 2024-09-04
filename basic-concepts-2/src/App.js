5e84e78e7a450b2dfd47397d412b213652f94969
VedulaLathasri <lathasri429@gmail.com>
2024-09-04 17:25:06 +0530

    Attaching Refs to DOM elements

--- basic-concepts-2/src/App.js
+++ basic-concepts-2/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Fragments from './components/Fragment/Fragments';
 // import ParentComp from './components/Purecomp/ParentComp';
 import ParentComp from './components/Memo/ParentComp';
+import RefsDemo from './components/Refs/RefsDemo';
 
 function App() {
   return (
     <div className="App">
+      <RefsDemo/>
       {/* <Fragments/> */}
-      <ParentComp/>
+      {/* <ParentComp/> */}
       {/* <MyPureComp/> */}
     </div>
   );
