e3962890a2b15e6f0d1416fa6a4e64b093a651f1
VedulaLathasri <lathasri429@gmail.com>
2024-09-04 18:32:27 +0530

    Refs with Class components

--- basic-concepts-2/src/App.js
+++ basic-concepts-2/src/App.js
@@ -4,11 +4,13 @@ import Fragments from './components/Fragment/Fragments';
 // import ParentComp from './components/Purecomp/ParentComp';
 import ParentComp from './components/Memo/ParentComp';
 import RefsDemo from './components/Refs/RefsDemo';
+import FocusInput from './components/RefsWithClassComp/FocusInput';
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo/>
+      <FocusInput/>
+      {/* <RefsDemo/> */}
       {/* <Fragments/> */}
       {/* <ParentComp/> */}
       {/* <MyPureComp/> */}
