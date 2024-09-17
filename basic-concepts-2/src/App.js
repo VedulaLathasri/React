dca128aac17568f5e35e989ca69a19db467c7bca
VedulaLathasri <lathasri429@gmail.com>
2024-09-17 16:00:10 +0530

    portal Demo

--- basic-concepts-2/src/App.js
+++ basic-concepts-2/src/App.js
@@ -5,11 +5,13 @@ import Fragments from './components/Fragment/Fragments';
 import ParentComp from './components/Memo/ParentComp';
 import RefsDemo from './components/Refs/RefsDemo';
 import FocusInput from './components/RefsWithClassComp/FocusInput';
+import PortalDemo from './components/Portals/PortalDemo';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      <PortalDemo/>
+      {/* <FocusInput/> */}
       {/* <RefsDemo/> */}
       {/* <Fragments/> */}
       {/* <ParentComp/> */}
